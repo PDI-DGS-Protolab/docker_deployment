562d2a328d0c49a2afeb359c31d42b331edc20d2
CarlosPerson <carlos.person.haro@gmail.com>
2014-01-10 13:16:59 +0100

    Changed Dockerfile download for DockerSetup folder download and added port redirection parameter

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-searching="Dockerfile"
-if echo "$1" | grep -q "$searching"; then
-  echo "matched"
-  newString=raw
-  wget `sed "s/blob/$newString/g" <<<"$1"`
-else
-  echo "no match"
-  wget $1/raw/$2/Dockerfile
-fi
+IFS=/ read -a SPLIT_URL <<< "$1"
+PROJECT_NAME="${SPLIT_URL[${#SPLIT_URL[@]}-1]}"
+sudo rm -rf "$PROJECT_NAME"
+git clone -b "$2" "$1"
+cd "$PROJECT_NAME"/DockerSetup
 sudo docker build -t $3 .
-sudo docker run -d -p 8000:8000 $3
+sudo docker run -d -p "$4":"$5" $3
--- autoDeployIni.sh
+++ autoDeployIni.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-expected_args=5	
+expected_args=7	
 if [ "$#" -ne "$expected_args" ]; then
-  	echo "Usage: autoDeploy.sh GITHUB_URL BRANCH CONTAINER_TAG SSH_ADDRESS SSH_CREDENTIALS"
+  	echo "Usage: autoDeploy.sh GITHUB_URL BRANCH CONTAINER_TAG LOCAL_PORT CONTAINER_PORT SSH_ADDRESS SSH_CREDENTIALS"
   	exit 1
 else
-	cat autoDeploy.sh | ssh $4 -i $5 "cat > /tmp/autoDeploy.sh ; chmod 755 /tmp/autoDeploy.sh ; /tmp/autoDeploy.sh $1 $2 $3"
+	cat autoDeploy.sh | ssh $6 -i $7 "cat > /tmp/autoDeploy.sh ; chmod 755 /tmp/autoDeploy.sh ; /tmp/autoDeploy.sh $1 $2 $3 $4 $5"
 	exit
 fi
