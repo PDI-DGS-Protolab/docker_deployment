0893d6d303267ba8a7be63d7bfd2e35343d9400d
recogemoras <recogemoras@gmail.com>
2014-01-15 09:48:27 +0100

    modified Ini so it could take a configuration file. autoDeployParams.conf holds place for all the configuration parameters in order to execute the script

--- autoDeployIni.sh
+++ autoDeployIni.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 expected_args=7	
-GITHUB_URL="$1";BRANCH="$2";CONT_TAG="$3";LOCAL_PORT="$4";CONT_PORT="$5";SSH_ADDR="$6";SSH_CREDS="$7";ENV_VARS="$8";
-if [ "$#" -lt "$expected_args" ]; then
-  	echo "Usage: autoDeploy.sh GITHUB_URL BRANCH CONTAINER_TAG LOCAL_PORT CONTAINER_PORT SSH_ADDRESS SSH_CREDENTIALS [ENVIRONMENT_VARS]"
+CONFIG_FILE=autoDeployParams.conf
+#reading the file
+if [[ -f $CONFIG_FILE ]]; then
+        . $CONFIG_FILE
+fi
+PARAMS=`wc -l < $CONFIG_FILE`
+if [ $PARAMS -lt "$expected_args" ]; then
+  	echo "Usage: autoDeploy.sh || configure parameters GITHUB_URL BRANCH CONTAINER_TAG LOCAL_PORT CONTAINER_PORT SSH_ADDRESS SSH_CREDENTIALS [ENVIRONMENT_VARS] in autoDeployParams.conf"
   	exit 1
 else
-	if [ "$#" -gt "$expected_args" ]; then 
+	if [ $PARAMS -gt "$expected_args" ]; then 
 		cat autoDeploy.sh | ssh $SSH_ADDR -i $SSH_CREDS "cat > /tmp/autoDeploy.sh ; 
 		chmod 755 /tmp/autoDeploy.sh ; /tmp/autoDeploy.sh $GITHUB_URL $BRANCH $CONT_TAG $LOCAL_PORT $CONT_PORT $ENV_VARS"
 	else 
